f2c4705cc6d6761b158461c6568e858d896ba604
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-05-15 07:51:57 -0600

    Updated

--- source/prep.specs/ReadOnlySetSpecs.cs
+++ source/prep.specs/ReadOnlySetSpecs.cs
@@ -1,34 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
+﻿using System.Collections.Generic;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
 using prep.collections;
-using Rhino.Mocks.Impl;
 
 namespace prep.specs
 {
-    class ReadOnlySetSpecs
+  class ReadOnlySetSpecs
+  {
+    internal class concern_for_readonly_set : Observes<IEnumerable<int>, ReadOnlySet<int>>
     {
-        public class concern_for_ReadOnly : Observes<IEnumerable<double>, ReadOnlySet<double>>
-        {
-            
-        }
-
-        public class when_ReadOnly_is_instantiated : concern_for_ReadOnly
-        {
-            private Establish c = () => items = depends.on<IEnumerable<double>>();
-
-            It should_have_the_double_set = () => items.Equals(new List<double>(){1,2,3,4});
-
-            static IEnumerable<double> items = new List<double>(){1,2,3,4};
-        }
-
-        public class when_getting_the_Enumerable : concern_for_ReadOnly
-        {
-            
-        }
     }
+
+    public class when_iterating_a_read_only_set : concern_for_readonly_set
+    {
+      //Do your setup here
+      Establish c = () =>
+      {
+        depends.on<IEnumerable<int>>(new List<int> {1, 2, 3, 4});
+        numbers = new List<int>();
+      };
+
+      //Invoke the functionality you are testing here
+      Because b = () =>
+      {
+        foreach (var item in sut)
+          numbers.Add(item);
+      };
+
+      //Make assertions in one or many it blocks
+      It should_iterate_all_of_the_items_it_was_provided = () =>
+        numbers.ShouldContain(1, 2, 3, 4);
+
+      static List<int> numbers;
+    }
+
+    public class when_getting_the_Enumerable : concern_for_readonly_set
+    {
+    }
+
+  }
 }
